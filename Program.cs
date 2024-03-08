dfaa4b1f4ab4ed43a43db1d1d63651a5cd3a1f27
ba <ba@QTARI>
2024-03-08 20:36:01 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Coding_Exercise___Two_Sum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Greetings, and Welcome to the Coding Exercise!\n");
+
+            Console.Write("Please enter the quantity of integers you intend to input: ");
+            int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+
+            int[] nums = new int[lengthOfArray];
+            Console.Write("Please enter the target integer: ");
+            int target = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"Enter the integer for #{i + 1}: ");
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        Console.WriteLine($"The output is: [{i},{j}]");
+                    }
+                }
+            }
+        }
+    }
+}
